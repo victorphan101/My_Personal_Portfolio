1df02a1ff07fa6f6ee2268334db2f9aaa785ddf8
Victor Phan <victor.work0809@gmail.com>
2024-01-28 02:53:23 -0800

    Skills component added

---
+++ src/components/skills/Other.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const Other = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Other</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                <i className="bx bx-badge-check"></i>
+
+                <div>
+                    <h3 className="skills__name">Microsoft Excel</h3>
+                    <span className="skills__level">Advanced</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i className="bx bx-badge-check"></i>
+
+                <div>
+                    <h3 className="skills__name">Google Sheets</h3>
+                    <span className="skills__level">Advanced</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i className="bx bx-badge-check"></i>
+
+                <div>
+                    <h3 className="skills__name">Figma</h3>
+                    <span className="skills__level">Advanced</span>
+                </div>
+                </div>
+            </div>
+
+            <div className="skills__group">
+                <div className="skills__data">
+                <i className="bx bx-badge-check"></i>
+
+                <div>
+                    <h3 className="skills__name">Linux</h3>
+                    <span className="skills__level">Basic</span>
+                </div>
+                </div>
+                <div className="skills__data">
+                <i className="bx bx-badge-check"></i>
+
+                <div>
+                    <h3 className="skills__name">Android Studio</h3>
+                    <span className="skills__level">Intermediate</span>
+                </div>
+                </div>
+
+                <div className="skills__data">
+                <i className="bx bx-badge-check"></i>
+
+                <div>
+                    <h3 className="skills__name">SCADA/MES</h3>
+                    <span className="skills__level">Basic</span>
+                </div>
+                </div>
+            </div>
+
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default Other
