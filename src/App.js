7edc54ccba945a0f5380d176a63e67e147619476
Victor Phan <victor.work0809@gmail.com>
2024-01-27 23:57:01 -0800

    Home component added and updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <>
       <Header/>
+      <main className="main">
+        <Home/>
+      </main>
     </>
   );
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/victor-phan-07b2951b1/" className="home__social-icon" target="_blank">
+            <i className="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/victorphan101" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
