c2162a1dcbc40f01fb7cd089afa7c60c363947d9
Victor Phan <victor.work0809@gmail.com>
2024-06-11 02:33:20 -0700

    PrimeTime hover effect

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Data = () => {
   return (
     <div className="home__data">
-        <h1 className="home__title">Victor Phan</h1>
+        <h1 className="home__title">Victor<span> Phan</span></h1>
 
         <h3 className="home__subtitle">Software Engineer</h3>
         <p className="home__description">I am a mobile applications and web developer in Orange County, California.</p>
